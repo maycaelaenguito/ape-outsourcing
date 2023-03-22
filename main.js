29f8b575a7d03f5a49293816dc17238554d34e7f
ReynaldArro <sleight.nald@gmail.com>
2023-03-22 16:53:22 +0800

    first commit

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-function navigation() {
-    var icon = document.getElementById("icon");
-    var navbar = document.getElementById("navbar");
 
-    if (icon.classList.contains('close-menu')) {
-        icon.classList.remove('close-menu');
-        icon.classList.add('open-menu');
-        navbar.style.left = '0';
-    } else {
-        icon.classList.remove('open-menu');
-        icon.classList.add('close-menu');
-        navbar.style.left = '-100%';
-    }
-
+var openNav = false;
+function navBtn()
+{
+  if(!openNav)
+  {
+    document.querySelector("#mobileNav").style.display = "block";
+    openNav = true;
+  }
+  else
+  {
+    document.querySelector("#mobileNav").style.display = "none";
+    openNav = false;
+  }
 }
+
+
+
+
 
