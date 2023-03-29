382d199c02c8325da3453ac861dc40c52042917b
maycaelaenguito <maycaelaenguito@gmail.com>
2023-03-29 14:50:20 -0400

    antok na ko

--- main.js
+++ main.js
@@ -1,41 +1,65 @@
-
+// NAVIGATION BAR
 function navigation() {
-    var navbar = document.getElementById("navbar");
-    var icon = document.getElementById("icon");
-  
-    if (navbar.style.left === "-100%") {
-      navbar.style.left = "0";
-      icon.classList.toggle("open-toggle_btn");
-    } else {
-      navbar.style.left = "-100%";
-      icon.classList.toggle("open-toggle_btn");
-    }
-  }
+  let navbar = document.getElementById("navbar");
+  let icon = document.getElementById("icon");
 
-  const sliderWrapper = document.querySelector('.slider-wrapper');
-  const sliderCards = document.querySelectorAll('.slider-card');
-  const prevBtn = document.querySelector('.prev');
-  const nextBtn = document.querySelector('.next');
-  const cardWidth = sliderCards[0].offsetWidth + 30;
-  let position = 0;
-  
-  prevBtn.addEventListener('click', () => {
-    position += cardWidth;
-    position = Math.min(position, 0);
-    sliderWrapper.style.transform = `translateX(${position}px)`;
-  });
-  
-  nextBtn.addEventListener('click', () => {
-    position -= cardWidth;
-    position = Math.max(position, -cardWidth * (sliderCards.length - 1));
-    sliderWrapper.style.transform = `translateX(${position}px)`;
-  });
-  
-  window.addEventListener('resize', () => {
-    position = 0;
-    cardWidth = sliderCards[0].offsetWidth + 30;
-    sliderWrapper.style.transform = `translateX(${position}px)`;
-  });
+  if (!navbar.style.left || navbar.style.left === "-100%") {
+    navbar.style.left = "0";
+    icon.classList.toggle("open-toggle_btn");
+  } else {
+    navbar.style.left = "-100%";
+    icon.classList.toggle("open-toggle_btn");
+  }
+}
+// REVIEWS CARD SLIDER
+const sliderWrapper = document.querySelector('.slider-wrapper');
+const sliderCards = document.querySelectorAll('.slider-card');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const cardWidth = sliderCards[0].offsetWidth + 30;
+let position = 0;
+let currentIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = Math.max(currentIndex - 1, 0);
+  position = -currentIndex * cardWidth;
+  sliderWrapper.style.transform = `translateX(${position}px)`;
+  nextBtn.disabled = false; 
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = Math.min(currentIndex + 1, sliderCards.length - 1);
+  position = -currentIndex * cardWidth;
+  sliderWrapper.style.transform = `translateX(${position}px)`;
+  if (currentIndex === sliderCards.length - 1) {
+    nextBtn.disabled = true; 
+  }
+  prevBtn.disabled = false; 
+});
+
+window.addEventListener('resize', () => {
+  currentIndex = 0;
+  position = 0;
+  cardWidth = sliderCards[0].offsetWidth + 30;
+  sliderWrapper.style.transform = `translateX(${position}px)`;
+  nextBtn.disabled = false; 
+  prevBtn.disabled = true; 
+});
+
+prevBtn.disabled = true; 
+
+// CLIENT LOGO SCROLLER
+function checkLogoRowWidth() {
+  var logoRow = document.querySelector('.logo-row');
+  if (logoRow.scrollWidth > logoRow.clientWidth) {
+    logoRow.classList.add('scrollable');
+  } else {
+    logoRow.classList.remove('scrollable');
+  }
+}
+
+window.addEventListener('load', checkLogoRowWidth);
+window.addEventListener('resize', checkLogoRowWidth);
 
 
 // var openNav = false;
