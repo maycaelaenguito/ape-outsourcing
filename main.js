934f864fe73ef2b6429e7b09d1ed04cd4df2a350
maycaelaenguito <maycaelaenguito@gmail.com>
2023-03-29 05:35:09 -0400

    modified homepage and deleted some irrelevant files

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 function navigation() {
-    var icon = document.getElementById("icon");
     var navbar = document.getElementById("navbar");
-
-    if (icon.classList.contains('close-menu')) {
-        icon.classList.remove('close-menu');
-        icon.classList.add('open-menu');
-        navbar.style.left = '0';
+    var icon = document.getElementById("icon");
+  
+    if (navbar.style.left === "-100%") {
+      navbar.style.left = "0";
+      icon.classList.toggle("open-toggle_btn");
     } else {
-        icon.classList.remove('open-menu');
-        icon.classList.add('close-menu');
-        navbar.style.left = '-100%';
+      navbar.style.left = "-100%";
+      icon.classList.toggle("open-toggle_btn");
     }
+  }
 
-}
-
+  const sliderWrapper = document.querySelector('.slider-wrapper');
+  const sliderCards = document.querySelectorAll('.slider-card');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+  const cardWidth = sliderCards[0].offsetWidth + 30;
+  let position = 0;
+  
+  prevBtn.addEventListener('click', () => {
+    position += cardWidth;
+    position = Math.min(position, 0);
+    sliderWrapper.style.transform = `translateX(${position}px)`;
+  });
+  
+  nextBtn.addEventListener('click', () => {
+    position -= cardWidth;
+    position = Math.max(position, -cardWidth * (sliderCards.length - 1));
+    sliderWrapper.style.transform = `translateX(${position}px)`;
+  });
+  
+  window.addEventListener('resize', () => {
+    position = 0;
+    cardWidth = sliderCards[0].offsetWidth + 30;
+    sliderWrapper.style.transform = `translateX(${position}px)`;
+  });
